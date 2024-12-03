16af89ea53561f5ed345a03ecfe6fb8ed93ec862
Wachira-Macharia <eugenecomboni@gmail.com>
2024-12-03 15:37:52 +0300

    Implementing Custom Permissions in Django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -15,10 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from .views import list_books, LibraryDetailView  # Importing the views explicitly
+from .views import list_books, LibraryDetailView, admin_view, librarian_view, member_view, add_book, edit_book, delete_book  # Importing the views explicitly
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('books/', list_books, name='list_books'),  # Function-based view for listing books
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # Class-based view for library details
+    path('admin/', admin_view, name='admin_view'),
+    path('librarian/', librarian_view, name='librarian_view'),
+    path('member/', member_view, name='member_view'),
+    path('book/add/', add_book, name='add_book'),
+    path('book/edit/<int:book_id>/', edit_book, name='edit_book'),
+    path('book/delete/<int:book_id>/', delete_book, name='delete_book'),
 ]
 
