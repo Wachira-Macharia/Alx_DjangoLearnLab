b8e24f3d773756412bcddce80b6f380a48df4e52
Wachira-Macharia <eugenecomboni@gmail.com>
2024-12-13 15:27:42 +0300

    Implementing a Custom User Model in Django

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import Book ,CustomUser
 
 # Custom admin configuration for the Book model
 class BookAdmin(admin.ModelAdmin):
@@ -8,3 +10,22 @@ class BookAdmin(admin.ModelAdmin):
     search_fields = ('title', 'author')
 
 admin.site.register(Book, BookAdmin)
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'date_of_birth', 'profile_photo')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'date_of_birth', 'profile_photo', 'is_staff', 'is_active'),
+        }),
+    )
+    list_display = ('email', 'is_staff', 'is_active')
+    search_fields = ('email',)
+    ordering = ('email',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
